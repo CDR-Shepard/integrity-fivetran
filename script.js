7d493a6c9e1bf8adb6f849c0a6a145e8240b9e9b
CDR-Shepard <56300999+CDR-Shepard@users.noreply.github.com>
2024-04-15 15:52:27 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ function connect(crm) {
+    if (crm === 'salesforce') {
+        window.open('https://fivetran.com/connect-card/setup?redirect_uri=https://dmforce.com/success&auth=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkIjp7ImxvZ2luIjp0cnVlLCJ1c2VyIjoiY2hlbWljYWxseV9wcm9mbGlnYWN5IiwiYWNjb3VudCI6InN3YXBwaW5nX3R1Y2tlZCIsImdyb3VwIjoidHJhbnNmZXJyZWRfaW5kZWxpYmxlIiwiY29ubmVjdG9yIjoic3RyYWlnaHRlbmVkX2FwaWVjZSIsIm1ldGhvZCI6IlBiZkNhcmQifSwiaWF0IjoxNzEzMjEwNjIzfQ.Jm3dvq7BwctQ4b0RLFvoEtChYSiqdcLcz81zTw5X02U&hide_setup_guide=true', '_blank');
+    } else {
+        console.log('Connect function for ' + crm + ' is not implemented yet.');
+    }
+}
+// v 0.01 04-15
